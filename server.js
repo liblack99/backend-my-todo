b4a23e09e9f0794c34812c43ed724cfc68b33fae
liblack <andresfc-@hotmail.com>
2024-12-24 03:37:10 -0500

    fixed(cors)

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 const corsOptions = {
-  origin: "http://localhost:5173",
+  origin: "*",
   methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"],
 };
