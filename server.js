4680ba40e8efe459ac1d08ddb673fb84065d2718
liblack <andresfc-@hotmail.com>
2024-12-25 06:08:58 -0500

    fixed(cors)

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 const corsOptions = {
-  origin: "https://my-todo-front.vercel.app/",
+  origin: "*",
   methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"],
 };
